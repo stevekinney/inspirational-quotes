fcf0d8eb4d751e92f1925577e8aef9598bfd21f0
Steve Kinney <hello@stevekinney.net>
2022-11-11 06:15:17 -0700

    Add count

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -10,11 +10,12 @@ export type Quote = {
 
 const Application = () => {
   const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [count, setCount] = useState(10);
 
   useEffect(() => {
     fetch('/api/quotes?limit=YOUR_COUNT_GOES_HERE')
       .then((res) => res.json())
-      .then(({ quotes }) => console.log(quotes));
+      .then(({ quotes }) => setQuotes(quotes));
   }, []);
 
   return (
