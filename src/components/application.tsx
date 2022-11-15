3ea117dccd738b4ec565f763455adb36c12a394a
Steve Kinney <hello@stevekinney.net>
2022-11-15 06:04:02 -0600

    Set up exercise

--- src/components/application.tsx
+++ src/components/application.tsx
@@ -9,30 +9,30 @@ export type Quote = {
   source?: string;
 };
 
-export const fetchRandomQuote = async () => {
-  const response = await fetch(`/api/quotes/random`);
-  return response.json();
-};
-
 export const fetchQuotes = async (count: number) => {
   const response = await fetch(`/api/quotes?limit=${count}`);
   return response.json();
 };
 
 const Application = () => {
-  const [quote, setQuote] = useState<Quote | undefined>();
+  const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
-    fetchRandomQuote().then(setQuote);
+    fetchQuotes(10).then(setQuotes);
   }, []);
 
-  if (!quote) return <Loading />;
+  if (!quotes.length) return <Loading />;
   return (
     <main className="w-full max-w-2xl py-16 mx-auto">
-      <InspirationalQuote content={quote.content} source={quote.source} />
-      {/* <Quotes>
-        <div className="grid grid-cols-2 gap-4"></div>
-      </Quotes> */}
+      <Quotes>
+        {/* {quotes.map((quote) => (
+          <InspirationalQuote
+            key={quote.id}
+            content={quote.content}
+            source={quote.source}
+          />
+        ))} */}
+      </Quotes>
     </main>
   );
 };
